45413667653fca9e74af4a9522e6c8448aee1a9d
firespiderOff <fireadam00010@gmail.com>
2025-11-20 20:48:32 +0100

    Update menu.py

--- manual/screens/menu.py
+++ manual/screens/menu.py
@@ -6,12 +6,13 @@ class MenuScreen:
     def __init__(self, goto_arena, goto_shop):
         self.elements = []
         # Load button images
-        normal_btn_img = load_asset("button.png")
-        hover_btn_img = load_asset("button.png")
 
+<<<<<<< HEAD
         # Buttons
         self.elements.append(Button((400, 200, 200, 50), goto_shop,
                                     normal_btn_img, hover_btn_img))
+=======
+>>>>>>> 1f0f6d36b5c56be8b3f56db7bac76b869d145dcc
     def handle_event(self, e):
         for el in self.elements: el.handle_event(e)
     def update(self, dt):
